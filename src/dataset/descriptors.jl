7b2bcca740cbf0792edd91e42a07f7baa1d6e3a6
giopaglia <24519853+giopaglia@users.noreply.github.com>
2021-12-04 01:46:03 +0100

    Add a more capable plotdescription

--- src/dataset/descriptors.jl
+++ src/dataset/descriptors.jl
@@ -1,4 +1,6 @@
 
+using DataStructures
+
 """
 TODO: docs
 # TODOs
@@ -10,79 +12,145 @@ TODO: docs
 """
 function plotdescription(
 	mfd::AbstractMultiFrameDataset;
-	descriptors::AbstractVector{Symbol} = [:mean_m],
+	descriptors::Union{AbstractVector{Symbol},AbstractDict{<:Any,<:AbstractVector{<:Symbol}}} = [:mean_m],
 	functions::AbstractVector{Function} = Function[var],
 	plot_kwargs::NamedTuple = NamedTuple(),
 	attribute_order::Symbol = :keep, # :increasing, :decreasing # TODO
 	layout::Symbol = :triangle, # :rectangle :pyramid
 	plot_dimesion::Symbol = :twoD, # :threeD # TODO
 	windows::AbstractVector{<:AbstractVector{<:AbstractVector{NTuple{3,Int}}}} =
-		[[[(t,0,0) for i in 1:d] for d in dimension(mfd)] for t in [1,2,4,8]]
+		[[[(t,0,0) for i in 1:d] for d in dimension(mfd)] for t in [1,2,4,8]],
+	on_x_axis = :attributes, # :
 )
 	# TODO: add assertions for Symbol kwargs
+	@assert windows == [[[(1,0,0)]]] "$(windows)"
+	@assert length(functions) == 1
+	@assert on_x_axis in [:descriptors, :attributes]
 
 	# concat symbols
 	cs(s1::Symbol, s2::Symbol) = Symbol(string(s1, "_", s2))
 	# blank plots
-	bp() = plot(; ticks = false, grid = false, axis = false)
+	bp() = plot() # plot(; ticks = false, grid = true, axis = true)
 	# count assigned values in vector
 	countassigned(v::AbstractVector) = length(findall([isassigned(v, i) for i in 1:length(v)]))
 
-	pyramid_base = maximum([t[1] for t in vcat(
+	singleton_groups = false
+	if isa(descriptors,AbstractVector{Symbol})
+		singleton_groups = true
+		descriptors = OrderedDict([string(descriptor) => [descriptor] for descriptor in descriptors])
+	end
+	# println(descriptors)
+	# Number of plots at the pyramid's base
+	pyramid_base_length = maximum([t[1] for t in vcat(
 		[d for w in windows for frame in w for d in w]...
 	)])
 
-	descriptions = Vector{Vector{DataFrame}}(undef, length(windows))
+	num_dimensional_frame = length(filter(x -> x isa Number && x > 0, dimension(mfd)))
+	n_attributes_per_frame = mfd[1:end].|>ncol
+	max_n_attributes = (n_attributes_per_frame)|>maximum
+	# println(num_dimensional_frame)
+	plot_pyramids = Array{Plots.Plot}(undef, length(windows), pyramid_base_length, (on_x_axis == :attributes ? length(descriptors) : max_n_attributes), num_dimensional_frame)
 
-	Threads.@threads for (i, win) in collect(enumerate(windows))
-		descriptions[i] = describe(mfd; desc = descriptors, t = win)
+	for i in 1:length(plot_pyramids)
+		plot_pyramids[i] = bp()
 	end
 
-	num_dimensional_frame = length(filter(x -> x isa Number && x > 0, dimension(mfd)))
+	stats = Vector{Vector{Vector{Vector{DataFrame}}}}(undef, length(descriptors))
 
-	plot_pyramids = Vector{Matrix{Plots.Plot}}(undef, num_dimensional_frame)
-	for (i_frame, dim) in enumerate(dimension(mfd))
-		if dim isa Symbol
-			continue
-			# throw(ErrorException("`plotdescription` still not implemented for `$(dim)` frames"))
-		elseif dim == 0
-			continue
-			# throw(ErrorException("`plotdescription` still not implemented for static frames"))
-		else
-			curr_pyramid = Matrix{Plots.Plot}(undef, length(windows), pyramid_base)
-			for (i_win, win) in enumerate(windows)
-				curr_frame_window = win[i_frame]
-				d = SoleBase.SoleData.SoleDataset._stat_description(
-					descriptions[i_win][i_frame];
-					functions = functions
-				)
-
-				for chunk in 1:pyramid_base
-					if chunk <= curr_frame_window[1][1] # curr_frame_window[1][1] is t
-						curr_pyramid[i_win, chunk] =
-							plot(
-								collect(1:nrow(d)),
-								# TODO: generalize on n-th function in functions
-								# TODO: generalize on m-th descriptor in descriptors
-								[v[chunk] for v in d[:,cs(descriptors[1], nameof(functions[1]))]],
-								title = "$(chunk) / $(curr_frame_window[1][1])",
-								xticks = (1:nrow(d), string.(d[:,1])),
-								xrotation = 65
-							)
-					else
-						# if layout == :triangle
-							curr_pyramid[i_win, chunk] = bp()
-						# end
-					end
-				end
-
+	# Load stats
+	for (i_descriptor_group,(descriptor_group_name,descrs)) in enumerate(descriptors)
+		println("Describing $(descriptor_group_name)...")
+		stats[i_descriptor_group] = Vector{Vector{DataFrame}}[]
+		for (i_descriptor,descriptor) in enumerate(descrs)
+			if !singleton_groups
+				println("\t$(descriptor)...")
 			end
-			plot_pyramids[i_frame] = curr_pyramid
+			_stats = Vector{DataFrame}[]
+			Threads.@threads for (i, win) in collect(enumerate(windows))
+				# SoleBase.describeonm(description; desc = [descriptor])
+				descriptions = SoleBase.describe(mfd; desc = [descriptor], t = win)
+				d = [SoleBase.SoleData.SoleDataset._stat_description(
+					description;
+					functions = functions,
+				) for description in descriptions]
+				push!(_stats, d)
+			end
+			push!(stats[i_descriptor_group], _stats)
 		end
 	end
 
+	# For each frame
+	for (i_frame, dim) in enumerate(dimension(mfd))
+		
+		if dim isa Symbol
+			# throw(ErrorException("`plotdescription` still not implemented for `$(dim)` frames"))
+			continue
+		elseif dim == 0
+			# throw(ErrorException("`plotdescription` still not implemented for static frames"))
+			continue
+		end
+
+		for (i_win, win) in enumerate(windows)
+			
+			curr_frame_window = win[i_frame]
+
+			if on_x_axis == :attributes
+				# For each descriptor
+				for (i_descriptor_group,(descriptor_group_name,descrs)) in enumerate(descriptors)
+					for (i_descriptor,descriptor) in enumerate(descrs)
+						d = stats[i_descriptor_group][i_descriptor][i_win][i_frame];
+						names = d[:,1]
+						n_attributes = nrow(d)
+						col = cs(descriptor, nameof(functions[1]))
+						
+						x = collect(1:n_attributes)
+						ys = d[:,col]
+
+						for i_chunk in 1:pyramid_base_length
+							plot!(plot_pyramids[i_win, i_chunk, i_descriptor_group, num_dimensional_frame],
+								x,
+								# TODO: generalize on n-th function in functions
+								[v[i_chunk] for v in ys],
+								labels = string(col),
+								title = "$(descriptor_group_name)$(pyramid_base_length == 1 ? "" : " $(i_chunk) / $(curr_frame_window[1][1])")",
+								xticks = (1:length(x), string.(names)),
+								xrotation = 65,
+							)
+						end
+					end
+				end
+			elseif on_x_axis == :descriptors
+
+				ds = [(stats[i_descriptor_group][i_descriptor][i_win][i_frame], descriptor) for (i_descriptor_group,(descriptor_group_name,descrs)) in enumerate(descriptors) for (i_descriptor,descriptor) in enumerate(descrs)];
+				n_descriptors = length(ds)
+
+				# println(ds)
+				# For each attribute
+				for i_attribute in 1:n_attributes_per_frame[i_frame]
+					x = collect(1:n_descriptors)
+					ys = [d[i_attribute, cs(descriptor, nameof(functions[1]))] for (d,descriptor) in ds]
+					names = [cs(descriptor, nameof(functions[1])) for (d,descriptor) in ds]
+					# println(ys)
+					for i_chunk in 1:pyramid_base_length
+						plot!(plot_pyramids[i_win, i_chunk, i_attribute, num_dimensional_frame],
+							x,
+							# TODO: generalize on n-th function in functions
+							[v[i_chunk] for v in ys],
+							labels = string("A$(i_attribute)"),
+							title = "A$(i_attribute)$(pyramid_base_length == 1 ? "" : " $(i_chunk) / $(curr_frame_window[1][1])")",
+							xticks = (1:length(x), string.(names)),
+							xrotation = 65,
+						)
+					end
+				end
+			end
+		end
+	end
+
+
 	# if layout == :triangle
-		return [plot(permutedims(pyr)..., layout = size(pyr)) for pyr in plot_pyramids]
+		# return [plot(permutedims(pyr)..., layout = size(pyr)) for pyr in plot_pyramids]
+	return plot_pyramids
 	# elseif layout == :rectangle
 	# 	res = Plots.Plot[]
 	# 	for pyr in plot_pyramids
