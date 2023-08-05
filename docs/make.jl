c820e75aab0b071ad3e66358a0433325d8b753fb
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-08-05 14:05:43 +0200

    Fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ DocMeta.setdocmeta!(SoleViz, :DocTestSetup, :(using SoleViz); recursive=true)
 
 makedocs(;
     modules=[SoleViz],
-    authors="Eduard I. STAN, Giovanni PAGLIARINI, Federico MANZELLA",
+    authors="Federico Manzella, Giovanni Pagliarini, Eduard I. Stan",
     repo="https://github.com/aclai-lab/SoleViz.jl/blob/{commit}{path}#{line}",
     sitename="SoleViz.jl",
     format=Documenter.HTML(;
@@ -20,5 +20,7 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/aclai-lab/SoleViz.jl",
-    devbranch="main",
+    target = "build",
+    branch = "gh-pages",
+    versions = ["main" => "main", "stable" => "v^", "v#.#", "dev" => "dev"],
 )
