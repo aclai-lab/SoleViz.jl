7f0b762691aaea6ea7594868ce96724e93136500
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-14 17:16:34 +1100

    Fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -6,9 +6,10 @@ DocMeta.setdocmeta!(SoleViz, :DocTestSetup, :(using SoleViz); recursive=true)
 makedocs(;
     modules=[SoleViz],
     authors="Federico Manzella, Giovanni Pagliarini, Eduard I. Stan",
-    repo="https://github.com/aclai-lab/SoleViz.jl/blob/{commit}{path}#{line}",
+    repo=Documenter.Remotes.GitHub("aclai-lab", "SoleViz.jl"),
     sitename="SoleViz.jl",
     format=Documenter.HTML(;
+        size_threshold = 4000000,
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://aclai-lab.github.io/SoleViz.jl",
         assets=String[],
